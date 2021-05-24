ff7d824c9cf9f9532485c2f3f81362fa0bac972b
Jackson Passos <jacksonguitar33@gmail.com>
2021-05-23 21:16:40 -0300

    :zap: content
    component content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,53 @@
-export function Content() {
-  // Complete aqui
+import { useState, useEffect } from 'react'
+import { api } from '../services/api'
+
+import '../styles/content.scss'
+
+import { MovieCard } from '../components/MovieCard';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  id: number,
+  title: string
+}
+
+
+export function Content(props: ContentProps) {
+  const { id, title } = props
+
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${id}`).then(response => {
+      setMovies(response.data);
+    });
+
+  }, [id]);
+
+  return (
+    <>
+      <header>
+        <span className="category">Categoria:<span> {title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </>
+  )
+
 }
